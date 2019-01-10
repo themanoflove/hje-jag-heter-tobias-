00a44b7e591b0a646d1b0e8ffae28688599350a6
tobias <tobias.gustafsson2@elev.ga.lbs.se>
2019-01-10 10:56:07 +0100

    hello my friend, this is my nie menu

---
+++ Assets/buttontoggle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttontoggle : MonoBehaviour {
+    
+
+	public void ToggleChange(bool state)
+    {
+        GetComponent<Button>().interactable = state;
+    }
+}
